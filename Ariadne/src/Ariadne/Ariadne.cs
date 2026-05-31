c98aed1ca99b100677d9d7fbc1ddf05d5a1be840
katx64 <ant.karathanasis15@gmail.com>
2026-05-31 08:21:58 +0300

    Add initial project files including .csproj, Program class, and Timer utility

---
+++ Ariadne/src/Ariadne/Ariadne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ariadne
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ariadne!");
+        }
+    }
+}
