479bd80a80664da3d678c3a40e3005d48c9ce0a2
katx64 <ant.karathanasis15@gmail.com>
2026-06-01 08:04:19 +0300

    Add abstract Algorithm class and initial BreadthFirst implementation

---
+++ Ariadne/src/Ariadne/Core/Algorithms/Uninformed/BreadthFirst.cs
@@ -0,0 +1,14 @@
+using Ariadne.Core.Structures;
+
+namespace Ariadne.Core.Algorithms.Uninformed
+{
+    internal class BreadthFirst : Algorithm
+    {
+        public BreadthFirst() : base() { }
+
+        public override void Search(Graph graph)
+        {
+            
+        }
+    }
+}
